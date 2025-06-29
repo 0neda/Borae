708c8de654199584f543fcbcc364aebcaf1561eb
GuilhermeSchweitzer <143623042+GuilhermeSchweitzer@users.noreply.github.com>
2025-06-29 01:43:49 -0300

    correção alpinejs mask

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,10 @@
 import "./app.css";
+import Alpine from 'alpinejs';
+import mask from '@alpinejs/mask';
+
+window.Alpine = Alpine;
+Alpine.plugin(mask);
+console.log("Plugin mask registrado:", Alpine);
+Alpine.start();
 
 console.log("Happy coding !!");
